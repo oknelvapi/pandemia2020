85a079f449eb086a60ee31439906af6ca506894a
IhorP <Ihor.Pavlenko@innovecs.com>
2020-03-19 13:43:09 +0200

    [pavlenko] add useContext and useReduser for settings

--- src/Components/Root/index.tsx
+++ src/Components/Root/index.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 
 import { Typography, Box } from '@material-ui/core/';
 
+import { SettingsProvider } from './settingsReducer';
+import Header from 'Components/Header';
+
 type RootProps = {
   children?: React.ReactElement;
 };
 
 const Root: React.FC<RootProps> = ({ children }: RootProps) => {
   return (
-    <Box height="1" width="1">
-      <Box component="header">
-        <Typography color="textPrimary">Header</Typography>
-      </Box>
-      <Box component="main">{children}</Box>
-      <div>
-        <Typography color="textPrimary">BottomNavigation</Typography>
-      </div>
-    </Box>
+    <>
+      <SettingsProvider>
+        <Box height="1" width="1">
+          <Header />
+          <Box component="main">{children}</Box>
+          <div>
+            <Typography color="textPrimary">BottomNavigation</Typography>
+          </div>
+        </Box>
+      </SettingsProvider>
+    </>
   );
 };
 
